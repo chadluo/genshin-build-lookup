316c5a1a5cd6ea199c83f0cb767abc1f277ff446
chadluo <i@yuki.rocks>
2022-01-17 02:05:01 +1100

    table view

--- q.js
+++ q.js
@@ -3,22 +3,14 @@ import { bosses, characters, domains, i18n, materials, weapons } from "./assets.
 const LANG = "zh_CN";
 
 export function byCharacter(character) {
-  return characters[character].materials.reduce(
-    (p, m) => ((p[formatMaterial(materials[m].name[LANG])] = find(m)), p),
-    {}
-  );
+  return characters[character].materials.reduce((p, m) => ((p[formatArray(materials[m].name[LANG])] = find(m)), p), {});
 }
 
-function formatMaterial(m) {
-  if (!Array.isArray(m)) {
-    return m;
-  }
-  return m.join(i18n.delimiter[LANG]);
+export function findCharacter(character) {
+  return characters[character].name[LANG];
 }
 
 function find(m) {
-  console.log(m);
-
   const ds = Object.values(domains)
     .filter((domain) => domain.materials_by_weekday.includes(m))
     .map((domain) => domain.name[LANG] + " " + findWeekday(domain.materials_by_weekday.indexOf(m)));
@@ -47,10 +39,36 @@ function byWeapon(weapon) {
   return w.materials.map((m) => materials[m].name["zh_CN"]);
 }
 
-console.log(byWeapon("Skyward Harp"));
+// console.log(byWeapon("Skyward Harp"));
 
 ///
 
 function byEnemy(enemy) {}
 
-console.log(byEnemy("Primo Geovishap"));
+// console.log(byEnemy("Primo Geovishap"));
+
+///
+
+/**
+ * object {
+ *    material: [enemy]
+ * }
+ */
+export function render(id, object) {
+  const materials = Object.keys(object);
+  return `<table>
+    <tr>
+      <td rowspan="${materials.length}">${id}</td>
+      <td>${formatArray(materials[0])}</td>
+      <td>${formatArray(object[materials[0]])}</td>
+    </tr>
+    ${materials
+      .slice(1)
+      .map((m) => `<tr><td>${formatArray(m)}</td><td>${formatArray(object[m])}</td></tr>`)
+      .join("")}
+  </table>`;
+}
+
+function formatArray(e) {
+  return Array.isArray(e) ? e.join(i18n.delimiter[LANG]) : e;
+}
