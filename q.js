0e9877a91ee2eb550d013e8a3e3c1a4d5ac394ef
chadluo <i@yuki.rocks>
2022-01-17 20:07:19 +1100

    query boss

--- q.js
+++ q.js
@@ -1,20 +1,111 @@
 import { bosses, characters, domains, i18n, materials, weapons } from "./assets.mjs";
 
+const output = document.querySelector("div.output");
+const query = document.getElementById("query");
 document.getElementById("lang-select").addEventListener("change", (event) => {
   document.documentElement.setAttribute("lang", event.target.value);
 });
 document.getElementById("byCharacter").addEventListener("click", () => {
-  document.querySelector("div.output").innerHTML += renderCharacter(document.getElementById("query").value);
+  output.innerHTML += renderCharacter(query.value);
 });
 document.getElementById("byWeapon").addEventListener("click", () => {
-  document.querySelector("div.output").innerHTML += renderWeapon(document.getElementById("query").value);
+  output.innerHTML += renderWeapon(query.value);
+});
+document.getElementById("byBoss").addEventListener("click", () => {
+  output.innerHTML += renderBoss(query.value);
 });
 
 function renderCharacter(character) {
   return render2(findCharacter(character), byCharacter(character));
 }
+
 function renderWeapon(weapon) {
   return render2(findWeapon(weapon), byWeapon(weapon));
+}
+
+function renderBoss(boss) {
+  return render2(findBoss(boss), byBoss(boss));
+}
+
+function findCharacter(character) {
+  return characters[character].name;
+}
+
+function byCharacter(character) {
+  return characters[character].materials.reduce(
+    (map, m) => (map.set(materials[m].name, findEnemiesForMaterial(m)), map),
+    new Map()
+  );
+}
+
+function findWeapon(weapon) {
+  return weapons[weapon].name;
+}
+
+function byWeapon(weapon) {
+  return weapons[weapon].materials.reduce(
+    (map, m) => (map.set(materials[m].name, findEnemiesForMaterial(m)), map),
+    new Map()
+  );
+}
+
+function findEnemiesForMaterial(m) {
+  const ds = Object.values(domains)
+    .filter((domain) => domain.materials_by_weekday.includes(m))
+    .map((domain) =>
+      Object.fromEntries(
+        Object.entries(domain.name).map(([lang, value]) => [
+          lang,
+          value + " " + findWeekday(lang, domain.materials_by_weekday.indexOf(m)),
+        ])
+      )
+    );
+
+  if (ds.length) return ds;
+
+  return Object.values(bosses)
+    .filter((b) => b.materials.includes(m))
+    .map((b) => b.name);
+}
+
+function findWeekday(lang, day) {
+  // prettier-ignore
+  switch (day) {
+    case 1: case 4: return i18n.weekdays.mon_thu[lang];
+    case 2: case 5: return i18n.weekdays.tue_fri[lang];
+    case 3: case 6: return i18n.weekdays.wed_sat[lang];
+    default:        return "";
+  }
+}
+
+function findBoss(boss) {
+  return bosses[boss].name;
+}
+
+function byBoss(boss) {
+  const ms = bosses[boss].materials;
+  const cs = ms.reduce(
+    (map, m) => (
+      map.set(materials[m].name, (map.get(materials[m].name) || []).concat(findCharactersForMaterial(m))), map
+    ),
+    new Map()
+  );
+  return ms.reduce(
+    (map, m) => (map.set(materials[m].name, (map.get(materials[m].name) || []).concat(findWeaponsForMaterial(m))), map),
+    cs
+  );
+}
+
+function findCharactersForMaterial(m) {
+  return Object.values(characters)
+    .filter((c) => c.materials.includes(m))
+    .map((c) => c.name);
+}
+
+function findWeaponsForMaterial(m) {
+  return Object.values(weapons)
+    .filter((w) => w.materials.includes(m))
+    .map((w) => w.name);
 }
 
 /**
@@ -36,71 +127,6 @@ function render2(id, object) {
       .join("")}
   </table>`;
 }
-
-function findCharacter(character) {
-  return characters[character].name;
-}
-
-function byCharacter(character) {
-  return characters[character].materials.reduce(
-    (map, m) => (map.set(materials[m].name, findEnemyForMaterial(m)), map),
-    new Map()
-  );
-}
-
-function findWeapon(weapon) {
-  return weapons[weapon].name;
-}
-
-function byWeapon(weapon) {
-  return weapons[weapon].materials.reduce(
-    (map, m) => (map.set(materials[m].name, findEnemyForMaterial(m)), map),
-    new Map()
-  );
-}
-
-function findEnemyForMaterial(m) {
-  const ds = Object.values(domains)
-    .filter((domain) => domain.materials_by_weekday.includes(m))
-    .map((domain) =>
-      Object.fromEntries(
-        Object.entries(domain.name).map(([lang, value]) => [
-          lang,
-          value + " " + findWeekday(lang, domain.materials_by_weekday.indexOf(m)),
-        ])
-      )
-    );
-
-  if (ds.length) return ds;
-
-  return Object.values(bosses)
-    .filter((b) => b.materials.includes(m))
-    .map((b) => b.name);
-}
-
-function findCharacterForMaterial(m) {
-  return Object.values(characters)
-    .filter((c) => c.materials.includes(m))
-    .map((c) => c.name);
-}
-
-function findWeekday(lang, day) {
-  // prettier-ignore
-  switch (day) {
-    case 1: case 4: return i18n.weekdays.mon_thu[lang];
-    case 2: case 5: return i18n.weekdays.tue_fri[lang];
-    case 3: case 6: return i18n.weekdays.wed_sat[lang];
-    default:        return "";
-  }
-}
-
-///
-
-function byEnemy(enemy) {}
-
-// console.log(byEnemy("Primo Geovishap"));
-
-///
 
 function formatArray(e) {
   return Array.isArray(e) ? e.map(formatName).join("<br>") : formatName(e);
