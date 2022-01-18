89ca268522c139d685dce1c73a55feaea9409aaa
chadluo <i@yuki.rocks>
2022-01-18 22:43:41 +1100

    enemy/domain table & query

--- q.js
+++ q.js
@@ -2,10 +2,15 @@ import { bosses, characters, domains, i18n, materials, weapons } from "./assets.
 
 const CHARACTERS = "characters";
 const WEAPONS = "weapons";
+const TROUNCE_DOMAINS = "trounce_domains";
+const ENEMIES = "enemies";
+const TALENT_DOMAINS = "talent_domains";
+const WEAPON_DOMAINS = "weapon_domains";
 
 const selectors = document.querySelector("div.selectors");
 selectors.innerHTML += renderItemsTable(characters, CHARACTERS);
 selectors.innerHTML += renderItemsTable(weapons, WEAPONS);
+selectors.innerHTML += renderEnemiesTable();
 
 const output = document.querySelector("div.output");
 const query = document.getElementById("query");
@@ -19,37 +24,97 @@ function renderItemsTable(items, title) {
     acc[key].push([id, obj]);
     return acc;
   }, {});
-
   const titleName = formatName(i18n[title]);
-
   return `<details open><summary>${titleName}</summary>
-  <table class="ctable" data-type="${title}"><tr><th>${formatName(i18n.rarity)}</th><th>${titleName}</th></tr>
+  <table class="ctable"><tr><th>${formatName(i18n.rarity)}</th><th>${titleName}</th></tr>
    ${Object.keys(byRarity)
      .sort()
      .reverse()
      .map(
        (rarity) =>
          `<tr><td>${rarity}</td><td>${byRarity[rarity]
-           .map(([id, obj]) => createLink(id, obj))
+           .map(([id, obj]) => createLink(id, title, obj.name))
            .join(formatName(i18n.delimiter))}</td></tr>`
      )
      .join("")}</table></details>`;
 }
 
-function createLink(id, item) {
-  return `<a data-id='${id}'>${formatName(item.name)}</a>`;
+function renderEnemiesTable() {
+  const trounce_domains = [
+    "Confront Stormterror",
+    "Enter the Golden House",
+    "Narukami Island: Tenshukaku",
+    "Wolf of the North Challenge",
+    "Beneath the Dragon-Queller",
+  ];
+  const enemies = [
+    "Anemo Hypostasis",
+    "Bathysmal Vishap Herd",
+    "Cryo Hypostasis",
+    "Cryo Regisvine",
+    "Electro Hypostasis",
+    "Geo Hypostasis",
+    "Golden Wolflord",
+    "Hydro Hypostasis",
+    "Maguu Kenki",
+    "Perpetual Mechanical Array",
+    "Primo Geovishap",
+    "Pyro Hypostasis",
+    "Pyro Regisvine",
+    "Rhodeia of Loch",
+    "Thunder Manifestation",
+  ];
+  const talent_domains = ["Forsaken Rift", "Taishan Mansion", "Violet Court"];
+  const weapon_domains = ["Cecilia Garden", "Hidden Palace of Lianshan Formula", "Court of Flowing Sand"];
+  return `<details open><summary>Enemies</summary>
+  <table class="ctable"><tr><th>Type</th><th>Enemies</th></tr>
+    <tr><th>Trounce domains</th><td>${trounce_domains
+      .map((d) => createLink(d, TROUNCE_DOMAINS, bosses[d].name))
+      .join(formatName(i18n.delimiter))}</td></tr>
+    <tr><th>Enemies</th><td>${enemies
+      .map((d) => createLink(d, ENEMIES, bosses[d].name))
+      .join(formatName(i18n.delimiter))}</td></tr>
+    <tr><th>Talent domains</th><td>${formatDomains(talent_domains, TALENT_DOMAINS)}</td></tr>
+    <tr><th>Weapon domains</th><td>${formatDomains(weapon_domains, WEAPON_DOMAINS)}</td></tr>
+  </table></details>`;
+}
+
+function createLink(id, type, names) {
+  return `<a data-id='${id}' data-type='${type}'>${formatName(names)}</a>`;
+}
+
+function formatDomains(ids, type) {
+  return ids
+    .map(
+      (d) =>
+        formatName(domains[d].name) +
+        " / " +
+        ["mon_thu", "tue_fri", "wed_sat"]
+          .map(
+            (weekday, i) =>
+              `<a data-id='${d}' data-weekday='${i + 1}' data-type='${type}'>${formatName(i18n.weekdays[weekday])}</a>`
+          )
+          .join(formatName(i18n.delimiter))
+    )
+    .join("<br>");
 }
 
 selectors.addEventListener("click", (event) => {
-  const path = event.composedPath();
-  const table = path.find((e) => e.tagName === "TABLE");
-  const a = path.find((e) => e.tagName === "A");
-  switch (table.dataset.type) {
+  const a = event.composedPath().find((e) => e.tagName === "A");
+  switch (a.dataset.type) {
     case CHARACTERS:
       output.innerHTML += renderCharacter(a.dataset.id);
       break;
     case WEAPONS:
       output.innerHTML += renderWeapon(a.dataset.id);
+      break;
+    case TROUNCE_DOMAINS:
+    case ENEMIES:
+      output.innerHTML += renderBoss(a.dataset.id);
+      break;
+    case TALENT_DOMAINS:
+    case WEAPON_DOMAINS:
+      output.innerHTML += renderDomain(a.dataset.id, a.dataset.weekday);
       break;
   }
 });
@@ -67,7 +132,7 @@ document.getElementById("byBoss").addEventListener("click", () => {
   output.innerHTML += renderBoss(query.value);
 });
 document.getElementById("byDomain").addEventListener("click", () => {
-  output.innerHTML += renderDomain(query.value);
+  output.innerHTML += renderDomain2(query.value);
 });
 document.getElementById("clear").addEventListener("click", () => {
   output.innerHTML = "";
@@ -85,10 +150,14 @@ function renderBoss(boss) {
   return render2(findBoss(boss), byBoss(boss));
 }
 
-function renderDomain(domain) {
+function renderDomain2(domain) {
   const inputs = domain.split(",");
   const domainName = inputs[0].trim();
   const weekday = parseInt(inputs[1].trim(), 10);
+  return renderDomain(domainName, weekday);
+}
+
+function renderDomain(domainName, weekday) {
   return render2(findDomain(domainName, weekday), byDomain(domainName, weekday));
 }
 
@@ -194,7 +263,7 @@ function render2(id, object) {
   const materials = Array.from(object.keys());
   return `<table class="qtable">
     <tr>
-      <th rowspan="${materials.length}">${formatName(id)}</th>
+      <th rowspan="${materials.length}">${formatName(id).replaceAll(" / ", "<br>")}</th>
       <td>${formatName(materials[0])}</td>
       <td>${formatArray(object.get(materials[0]))}</td>
     </tr>
