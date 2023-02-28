b3522fe7634a5d7fd3b10277e63a089ddb5ce934
chadluo <i@yuki.rocks>
2023-02-28 22:27:39 +1100

    activate 3.5

--- src/version.ts
+++ src/version.ts
@@ -1,7 +1,8 @@
 const version: Record<string, string[]> = {
   "3.4": ["Alhaitham", "Yaoyao", "Light of Foliar Incision"],
   "3.5": ["Dehya", "Mika", "Beacon of the Reed Sea", "Mailed Flower"],
+  "3.6": [],
 };
 
-export const recent_new = version["3.4"];
-export const upcoming = version["3.5"];
+export const recent_new = version["3.5"];
+export const upcoming = version["3.6"];
