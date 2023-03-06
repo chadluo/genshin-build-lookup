5062dfbf773fee1e023595993ecb7311bbfde17a
chadluo <i@yuki.rocks>
2023-03-06 21:49:49 +1100

    indicate timezone by Earth Globe emoji

--- src/components/todaytable.ts
+++ src/components/todaytable.ts
@@ -2,8 +2,8 @@ import { byDomain, findDomain, formatName, formatTableCaption, renderQTableRows 
 import { i18n, weekdays } from "../i18n";
 import { domains } from "../models/enemies";
 
-type TimezoneNames = "Asia" | "Europe" | "America";
-const timezones: { [tz in TimezoneNames]: number } = { Asia: 8, Europe: 1, America: -5 };
+type Timezone = "Asia" | "Europe" | "America";
+const timezones: { [tz in Timezone]: number } = { Asia: 8, Europe: 1, America: -5 };
 
 export function define() {
   customElements.define(
@@ -15,7 +15,7 @@ export function define() {
         const weekdays = day === 0 ? [1, 2, 3] : [day];
         this.innerHTML = `<details class="section" ${day === 0 ? "" : "open"}>
         <summary>${formatTableCaption("today")}</summary>
-        <div class="timezone-selector">${(["Asia", "Europe", "America"] as TimezoneNames[])
+        <div class="timezone-selector">${(["Asia", "Europe", "America"] as Timezone[])
           .map(
             (zone) =>
               `<label><input type="radio" name="timezone" value="${zone}" ${
@@ -28,17 +28,19 @@ export function define() {
       }
 
       getTimezone() {
-        return (localStorage.getItem("timezone") as TimezoneNames) || this.guessTimezone();
+        return (localStorage.getItem("timezone") as Timezone) || this.guessTimezone();
       }
 
-      formatZoneOption(zone: TimezoneNames) {
-        return formatName(i18n[zone]) + formatName(i18n.delimiter) + formatName(weekdays[this.getWeekday(zone)]);
+      formatZoneOption(zone: Timezone) {
+        return `${this.getTimezoneIcon(zone)} ${formatName(i18n[zone])}${formatName(i18n.delimiter)}${formatName(
+          weekdays[this.getWeekday(zone)]
+        )}`;
       }
 
       refreshDomains(event: Event) {
         const target = event.target as HTMLInputElement;
         if (target.name === "timezone") {
-          const timezone = target.value as TimezoneNames;
+          const timezone = target.value as Timezone;
           const day = this.getWeekday(timezone);
           const weekdays = day === 0 ? [1, 2, 3] : [day];
           this.querySelector("table.qtable")!.innerHTML = this.renderDomains(weekdays);
@@ -73,19 +75,29 @@ export function define() {
        * @see https://genshin.hoyoverse.com/en/news/detail/6638
        * @returns weekday at specific server timezone, range [0..3]
        */
-      getWeekday(zone: TimezoneNames) {
+      getWeekday(zone: Timezone) {
         const now = new Date();
-        const utcH = now.getUTCHours();
+        const utcHour = now.getUTCHours();
         const utcDay = now.getUTCDay();
-        const zoneH = utcH + timezones[zone];
+        const zoneH = utcHour + timezones[zone];
         // refresh at 4am
         const zoneDay = ((zoneH > 28 ? utcDay + 1 : zoneH < 4 ? utcDay - 1 : utcDay) + 7) % 7;
         return zoneDay > 3 ? zoneDay - 3 : zoneDay;
       }
 
-      guessTimezone(): TimezoneNames {
+      guessTimezone(): Timezone {
         const offset = -new Date().getTimezoneOffset() / 60;
         return offset < -2 ? "America" : offset < 4.5 ? "Europe" : "Asia";
+      }
+
+      getTimezoneIcon(zone: Timezone) {
+        // prettier-ignore
+        switch (zone) {
+          case "Asia":    return "🌏";
+          case "Europe":  return "🌍";
+          case "America": return "🌎";
+          default:        return undefined;
+        }
       }
     }
   );
